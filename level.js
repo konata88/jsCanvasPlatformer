b5a96a8489b5d5ab6c6c006e770a1ce725b5b56d
roman <teplitsky@runnet.ru>
2022-03-06 11:37:22 +0300

    fixed

--- level.js
+++ level.js
@@ -1,29 +1,30 @@
-const obst = []
-obst.push({x:4, y:2, halfX:4, halfY:2})
-obst.push({x:12, y:2, halfX:4, halfY:2})
-obst.push({x:20, y:2, halfX:4, halfY:2})
-obst.push({x:28, y:2, halfX:4, halfY:2})
-obst.push({x:36, y:2, halfX:4, halfY:2})
-obst.push({x:44, y:2, halfX:4, halfY:2})
-obst.push({x:52, y:2, halfX:4, halfY:2})
-obst.push({x:60, y:2, halfX:4, halfY:2})
-obst.push({x:68, y:2, halfX:4, halfY:2})
-obst.push({x:76, y:2, halfX:4, halfY:2})
-obst.push({x:84, y:2, halfX:4, halfY:2})
-obst.push({x:92, y:2, halfX:4, halfY:2})
-obst.push({x:100, y:2, halfX:4, halfY:2})
-obst.push({x:108, y:2, halfX:4, halfY:2})
-obst.push({x:116, y:2, halfX:4, halfY:2})
-obst.push({x:124, y:2, halfX:4, halfY:2})
-obst.push({x:132, y:2, halfX:4, halfY:2})
+const obst = [];
+obst.push({x:4, y:2, halfX:4, halfY:2});
+obst.push({x:12, y:2, halfX:4, halfY:2});
+obst.push({x:20, y:2, halfX:4, halfY:2});
+obst.push({x:28, y:2, halfX:4, halfY:2});
+obst.push({x:36, y:2, halfX:4, halfY:2});
+obst.push({x:44, y:2, halfX:4, halfY:2});
+obst.push({x:52, y:2, halfX:4, halfY:2});
+obst.push({x:60, y:2, halfX:4, halfY:2});
+obst.push({x:68, y:2, halfX:4, halfY:2});
+obst.push({x:76, y:2, halfX:4, halfY:2});
+obst.push({x:84, y:2, halfX:4, halfY:2});
+obst.push({x:92, y:2, halfX:4, halfY:2});
+obst.push({x:100, y:2, halfX:4, halfY:2});
+obst.push({x:108, y:2, halfX:4, halfY:2});
+obst.push({x:116, y:2, halfX:4, halfY:2});
+obst.push({x:124, y:2, halfX:4, halfY:2});
+obst.push({x:132, y:2, halfX:4, halfY:2});
 
 
 //obst.push({x:40, y:20, halfX:4, halfY:2})
-obst.push({x:48, y:20, halfX:4, halfY:2})
-obst.push({x:56, y:20, halfX:4, halfY:2})
-obst.push({x:64, y:20, halfX:4, halfY:2})
+obst.push({x:48, y:20, halfX:4, halfY:2});
+obst.push({x:56, y:20, halfX:4, halfY:2});
+obst.push({x:64, y:20, halfX:4, halfY:2});
 
-obst.push({x:30, y:40, halfX:4, halfY:2})
-obst.push({x:22, y:40, halfX:4, halfY:2})
-
+obst.push({x:30, y:40, halfX:4, halfY:2});
+obst.push({x:22, y:40, halfX:4, halfY:2});
+const moving = [];
+moving.push({x:0, y:20, halfX:4, halfY:2, axis: 'x', right: true, startX: 0, finishX: 20});
 //obst.push({x:40, y:4, halfX:4, halfY:4})
